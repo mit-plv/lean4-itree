725c21709620109a401ef581852a234cdc841eb5
Paul Mure <paulmure2000@gmail.com>
2023-10-02 19:03:26 -0400

    rework

--- Spacetalk/Basic.lean
+++ Spacetalk/Basic.lean
@@ -25,7 +25,9 @@ infix:67 " ++ " => HList.append
 -- Given a List α, a function f : α → β,
 -- return a HList with indices of type β and values of β-indexed type γ
 -- using the mapping function g : (a : α) → γ (f a).
-def HList.from_list {α : Type v1} {β : Type v2} {γ : β → Type u} (f : α → β) (g : (a : α) → γ (f a)) : (l : List α) → HList γ (l.map f)
+def HList.from_list {α : Type v1} {β : Type v2} {δ : β → Type u}
+                    (f : α → β) (g : (a : α) → δ (f a)) : 
+                    (l : List α) → HList δ (l.map f)
   | [] => []
   | h :: t => g h :: HList.from_list f g t
 
@@ -40,28 +42,8 @@ namespace Spatium
     | prod : Ty → Ty → Ty
     | fn : Ty → Ty → Ty
     | stream : Ty → Ty
-  infixr:55 " × " => Ty.prod
+  infixr:35 " × " => Ty.prod
   infixr:25 " → " => Ty.fn
-
-  inductive BinOp : Ty → Ty → Type
-    | plus : BinOp (.nat × .nat) .nat
-
-  -- Exprs operate on not streams
-  -- Progs operate on streams and are always stream(s) → stream(s)
-  -- This is so that a final program is always a function from stream(s) to stream(s)
-
-  inductive Expr : Ty → Type
-    | const : Nat → Expr .nat
-    | binop : BinOp α β → Expr (α → β)
-
-  -- Cannot encode filter on infinite streams
-  -- Filters must be done "locally"
-  inductive Prog : Ty → Type
-    | zip : Prog ((.stream α × .stream β) → .stream (α × β))
-    | map : Expr (α → β) → Prog (.stream α → .stream β)
-    | comp : Prog (α → β) → Prog (β → γ) → Prog (α → γ)
-
-  -- Semantics
 
   @[reducible] def Ty.denote : Ty → Type
     | nat => Nat
@@ -69,43 +51,97 @@ namespace Spatium
     | fn α β => α.denote → β.denote
     | stream ty => Stream' ty.denote
 
-  @[simp] def BinOp.denote : BinOp α β → (α.denote → β.denote)
-    | plus => λ (a, b) => a + b
+  inductive BinOp : Ty → Ty → Ty → Type
+    | add : BinOp .nat .nat .nat
+    | mul : BinOp .nat .nat .nat
+
+  -- Exprs operate on not streams
+  -- Progs operate on streams and are always stream(s) → stream(s)
+  -- This is so that a final program is always a function from stream(s) to stream(s)
+
+  inductive Expr : Ty → Type
+    | const : Nat → Expr .nat
+    | binop : BinOp α β δ → Expr (α → β → δ)
+
+  def a : Nat → Nat
+    | _ => 0
+  def b : Nat → Nat → Nat
+    | _ => 0
+  #check (b ∘ a : Nat → Nat → Nat)
+  #check Function.comp
+
+  -- Cannot encode filter on infinite streams
+  -- Filters must be done "locally"
+  inductive Prog (rep : Ty → Type) : Ty → Type
+    | var : rep ty → Prog rep ty
+    | lam : (rep dom → Prog rep ran) → Prog rep (dom → ran)
+    | app : Prog rep (dom → ran) → Prog rep dom → Prog rep ran
+    | zip : Expr (α → β → δ) → Prog rep (.stream α → .stream β → .stream δ)
+    | map : Expr (α → β) → Prog rep (.stream α → .stream β)
+    | reduce : Expr (α → β → α) → Nat → α.denote → Prog rep (.stream β → .stream α)
+    | comp : Prog rep (β → δ) → Prog rep (α → β) → Prog rep (α → δ)
+  infixr:90 " ∘ " => Prog.comp
+
+  -- Semantics
+
+  @[simp] def BinOp.denote : BinOp α β δ → (α.denote → β.denote → δ.denote)
+    | add => HAdd.hAdd
+    | mul => HMul.hMul
 
   @[simp] def Expr.denote : Expr ty → ty.denote
     | const n => n
     | binop op => op.denote
 
-  @[simp] def Prog.denote : Prog ty → ty.denote
-    | zip => λ (sa, sb) => Stream'.zip (·, ·) sa sb
-    | map f => λ s => Stream'.map f.denote s
-    | comp f g => g.denote ∘ f.denote
+  def Stream'.reduce (f : α → β → α) (dim : Nat) (a : α) (s : Stream' β) : Stream' α :=
+    λ n =>
+      let s_forwarded := s.drop (dim * n)
+      let reduction_list := s_forwarded.take dim
+      reduction_list.foldl f a
+
+  @[simp] def Prog.denote : Prog Ty.denote ty → ty.denote
+    | var x => x
+    | lam f => λ x => (f x).denote
+    | app f a => f.denote a.denote
+    | zip f => Stream'.zip f.denote
+    | map f => Stream'.map f.denote
+    | reduce f dim init => Stream'.reduce f.denote dim init
+    | comp f g => Function.comp f.denote g.denote
 
   namespace Example
-    -- A rudimentary a + b example
-    def adder : Prog ((.stream .nat × .stream .nat) → .stream .nat) :=
-      .comp  .zip (.map (.binop .plus))
+    -- A dot product of length n vectors
+    def dot_prod (n : Nat) : Prog rep (.stream .nat → .stream .nat → .stream .nat) :=
+      let multiply := .zip (.binop .mul)
+      let reduction := .reduce (.binop .add) n 0
+      .lam λ a => .lam λ b => .app reduction (.app (.app multiply (.var a)) (.var b))
 
-    def adder' (tup : Stream' Nat × Stream' Nat) : Stream' Nat :=
-      λ n => (tup.fst.nth n) + (tup.snd.nth n)
+    def dot_prod' (n : Nat) (a : Stream' Nat) (b : Stream' Nat) : Stream' Nat :=
+      Stream'.reduce (· + ·) n 0 (Stream'.zip (· * ·) a b)
 
     def sa : Stream' Nat := id
     def sb : Stream' Nat := id
 
-    def add := adder.denote (sa, sb)
-    def add' := adder' (sa, sb)
-    #eval add.nth 5
-    #eval add'.nth 5
+    def dp (n : Nat) := (dot_prod n).denote sa sb
+    def dp' (n : Nat) := dot_prod' n sa sb
+    def n := 10
+    #eval (dp n).nth 0
+    #eval (dp' n).nth 0
 
-    theorem adder_equiv : adder.denote = adder' := by
-      repeat {
-        apply funext
-        intros
-        simp [adder', Stream'.map, Stream'.zip, Stream'.nth]
-      }
+    theorem dp_equiv : ∀n : Nat, (dot_prod n).denote = dot_prod' n := by
+      simp [dot_prod']
   end Example
 
 end Spatium
+
+namespace HIR
+
+  inductive Ty
+    | nat
+  
+  inductive NodeOps : List Ty → List Ty → Type
+    | add : NodeOps [.nat, .nat] [.nat]
+  
+
+end HIR
 
 -- (Virtual) RDA Spec
 -- Q: What optimizations should we do at this level?
@@ -148,15 +184,15 @@ namespace VirtFlow
     producer : Fin num_nodes ⊕ External
     consumer : Fin num_nodes ⊕ External
 
-  def find_node_inputs (fifos : Vector (FIFO num_nodes) num_fifos) (id : Fin num_nodes) : List Ty :=
+  def FIFOList (num_nodes num_fifos : Nat) := Vector (FIFO num_nodes) num_fifos
+
+  def find_node_inputs (fifos : FIFOList num_nodes num_fifos) (id : Fin num_nodes) : List Ty :=
     let filtered := fifos.toList.filter (match ·.consumer with | .inl node_id => node_id == id | .inr _ => false)
     filtered.map (·.ty)
 
-  def find_node_outputs (fifos : Vector (FIFO num_nodes) num_fifos) (id : Fin num_nodes) : List Ty :=
+  def find_node_outputs (fifos : FIFOList num_nodes num_fifos) (id : Fin num_nodes) : List Ty :=
     let filtered := fifos.toList.filter (match ·.producer with | .inl node_id => node_id == id | .inr _ => false)
     filtered.map (·.ty)
-
-  def FIFOList (num_nodes num_fifos : Nat) := Vector (FIFO num_nodes) num_fifos
 
   -- The circuit is a steam → stream
   structure Node (fifos : FIFOList num_nodes num_fifos) (nid : Fin num_nodes) where
