bff5edf9a3dbb9793ab6a94c6577d3828ce2a625
Paul Mure <paulmure2000@gmail.com>
2024-04-16 10:09:46 -0400

    update

--- Spacetalk/Compiler.lean
+++ Spacetalk/Compiler.lean
@@ -10,78 +10,96 @@ def Step.Prim.toSDF : Step.Prim → SimpleDataflow.Ty
 def Step.Prim.toSDFOpt : Step.Prim → SimpleDataflow.Ty
   | .bitVec w => .option (.bitVec w)
 
+structure SDFOneOutput (p : Step.Prim) where
+  g : SimpleDataflow.DataflowMachine
+  fifo : OutputFIFO g.outputs g.nodes
+  idx : Member (FIFO.output fifo) g.fifos
+  h_ty : fifo.t = p.toSDF
+
+@[reducible]
 def Step.Ty.toSDF (sty : Step.Ty) :=
   match sty with
-    | .stream p => ([], [p.toSDF])
-    | .fn α β =>
-      let (ai, ao) := α.toSDF
-      let (bi, bo) := β.toSDF
-      
+    | .stream p => SDFOneOutput p
 
-def c : SimpleDataflow.Pipeline [] [SimpleDataflow.BoolTy] :=
-  .const 1
-
-def g : SimpleDataflow.DataflowMachine :=
-  let inputs : List SimpleDataflow.Ty := []
-  let outputs : List SimpleDataflow.Ty := [SimpleDataflow.BoolTy]
-  let state : List SimpleDataflow.Ty := []
-  let pipeline : SimpleDataflow.Ops inputs outputs state := c
-  let node := ⟨inputs, outputs, state, []ₕ, pipeline⟩
-  let nodes : SDFNodeList 1 := .cons node .nil
-  let fifos := [
-    .output ⟨SimpleDataflow.BoolTy, 0, .head, .head⟩
-  ]
-  ⟨inputs, outputs, 1, nodes, fifos⟩
-
-def binOpConstRight (op : SimpleDataflow.BinaryOp α β γ) (c : β.denote) : SimpleDataflow.DataflowMachine :=
-  let constNode := ⟨[], [β], [], []ₕ, .const c⟩
-  let opNode := ⟨[α, β], [γ], [], []ₕ, .binaryOp op⟩
+@[simp]
+def binOpConstRightGraph (α β γ : Step.Prim) (op : SimpleDataflow.BinaryOp α.toSDF β.toSDF γ.toSDF) (c : β.denote) : SDFOneOutput γ :=
+  let inputs := [α.toSDF]
+  let outputs := [γ.toSDF]
+  let constNode := ⟨[], [β.toSDF], [], []ₕ, .const c⟩
+  let opNode := ⟨[α.toSDF, β.toSDF], [γ.toSDF], [], []ₕ, .binaryOp op⟩
   let nodes : SDFNodeList 2 := .cons constNode (.cons opNode .nil)
+  have h_eq : (nodes.get 1).outputs = [γ.toSDF] := by aesop
+  let outFifo : OutputFIFO outputs nodes := ⟨γ.toSDF, 1, h_eq ▸ .head, .head⟩
+  let fifo := .output outFifo
   let fifos := [
-    .input ⟨α, .head, 1, .head⟩,
-    .advancing ⟨β, 0, 1, .head, .tail .head, by simp⟩,
-    .output ⟨γ, 1, .head, .head⟩
+    .input ⟨α.toSDF, .head, 1, .head⟩,
+    .advancing ⟨β.toSDF, 0, 1, .head, .tail .head, by simp⟩,
+    fifo
   ]
-  ⟨[α], [γ], 2, nodes, fifos⟩
+  let g : SimpleDataflow.DataflowMachine := ⟨inputs, outputs, 2, nodes, fifos⟩
+  ⟨
+    g,
+    
+  ⟩
 
-namespace _hidden
-  def w := 10
-  def add1 := binOpConstRight (.add (w := w)) 1
-  def mul1 := binOpConstRight (.mul (w := w)) 1
-  def s : Stream' (BitVec w) := λ n => ⟨n % (2 ^ w), by apply Nat.mod_lt; simp⟩
-  #eval ((add1.denote [s]ₕ).head 8).toNat
-end _hidden
+def binaryOpGraph (op : SimpleDataflow.BinaryOp α β γ) : SimpleDataflow.DataflowMachine :=
+  let opNode := ⟨[α, β], [γ], [], []ₕ, .binaryOp op⟩
+  let nodes : SDFNodeList 1 := .cons opNode .nil
+  let fifos := [
+    .input ⟨α, .head, 0, .head⟩,
+    .input ⟨β, .tail .head, 0, .tail .head⟩,
+    .output ⟨γ, 0, .head, .head⟩
+  ]
+  ⟨[α, β], [γ], 1, nodes, fifos⟩
 
-def Step.UnaryOp.compile : Step.UnaryOp α β → SimpleDataflow.DataflowMachine
-  | .addConst c => binOpConstRight .add c
-  | .mulConst c => binOpConstRight .mul c
+def Step.UnaryOp.compile : Step.UnaryOp α β → SDFOneOutput β
+  | .addConst c => binOpConstRightGraph α α α .add c
+  | .mulConst c => binOpConstRightGraph α α α .mul c
 
-def Step.BinaryOp.compile : Step.BinaryOp α β γ → SimpleDataflow.Pipeline [α.toSDF, β.toSDF] [γ.toSDF]
-  | .add => .binaryOp .add
-  | .mul => .binaryOp .mul
+def Step.BinaryOp.compile : Step.BinaryOp α β γ → SDFOneOutput γ
+  | @Step.BinaryOp.add w => ⟨binaryOpGraph (@SimpleDataflow.BinaryOp.add w), by aesop⟩
+  | @Step.BinaryOp.mul w => ⟨binaryOpGraph (@SimpleDataflow.BinaryOp.mul w), by aesop⟩
 
-def Step.Prog.compile {sty : Step.Ty} : Step.Prog sty → SimpleDataflow.DataflowMachine
-  | @Step.Prog.zip α β γ op =>
-    let inputs : List SimpleDataflow.Ty := [α.toSDF, β.toSDF]
-    let ia : Member α.toSDF inputs := .head
-    let ib : Member β.toSDF inputs := .tail .head
-    let outputs : List SimpleDataflow.Ty := [γ.toSDF]
-    let ic : Member γ.toSDF outputs := .head
-    let state : List SimpleDataflow.Ty:= []
-    let pipeline : SimpleDataflow.Ops inputs outputs state := op.compile
-    let node := ⟨inputs, outputs, state, []ₕ, pipeline⟩
-    let nodes : SDFNodeList 1 := .cons node .nil
-    let fifos := [
-      .input ⟨α.toSDF, ia, 0, ia⟩,
-      .input ⟨β.toSDF, ib, 0, ib⟩,
-      .output ⟨γ.toSDF, 0, ic, ic⟩
-    ]
-    ⟨inputs, outputs, 1, nodes, fifos⟩
-  | Step.Prog.map op => op.compile
-  | @Step.Prog.reduce α β op len init => sorry
-  | @Step.Prog.comp α β γ f g =>
-    let fGraph := f.compile
-    let gGraph := g.compile
-    let ia : Member α.toSDF gGraph.inputs := .head
+def Step.Prog.compile {sty : Step.Ty} : Step.Prog sty → sty.toSDF
+  | @Step.Prog.const α as => sorry
+  | @Step.Prog.zip α β γ op as bs =>
+    let asg := as.compile
+    let bsg := bs.compile
+    let opg := op.compile
+    have h_op_inputs : opg.val.inputs = [α.toSDF, β.toSDF] := by
+      simp [opg, Step.BinaryOp.compile, binaryOpGraph]
+      aesop
+    let iap : Member α.toSDF asg.val.outputs := asg.property ▸ .head
+    let iac : Member α.toSDF opg.val.inputs := h_op_inputs ▸ .head
+    let ibp : Member β.toSDF bsg.val.outputs := bsg.property ▸ .head
+    let ibc : Member β.toSDF opg.val.inputs := h_op_inputs ▸ .tail .head
+
     sorry
-  | .comp2 f g => sorry
+  | Step.Prog.map op as => sorry
+  | @Step.Prog.reduce α β op len init bs => sorry
+  decreasing_by (all_goals sorry)
+
+
+
+-- def c : SimpleDataflow.Pipeline [] [SimpleDataflow.BoolTy] :=
+--   .const 1
+
+-- def g : SimpleDataflow.DataflowMachine :=
+--   let inputs : List SimpleDataflow.Ty := []
+--   let outputs : List SimpleDataflow.Ty := [SimpleDataflow.BoolTy]
+--   let state : List SimpleDataflow.Ty := []
+--   let pipeline : SimpleDataflow.Ops inputs outputs state := c
+--   let node := ⟨inputs, outputs, state, []ₕ, pipeline⟩
+--   let nodes : SDFNodeList 1 := .cons node .nil
+--   let fifos := [
+--     .output ⟨SimpleDataflow.BoolTy, 0, .head, .head⟩
+--   ]
+--   ⟨inputs, outputs, 1, nodes, fifos⟩
+
+-- namespace _hidden
+--   def w := 10
+--   def add1 := binOpConstRight (.add (w := w)) 1
+--   def mul1 := binOpConstRight (.mul (w := w)) 1
+--   def s : Stream' (BitVec w) := λ n => ⟨n % (2 ^ w), by apply Nat.mod_lt; simp⟩
+--   #eval ((add1.denote [s]ₕ).head 8).toNat
+-- end _hidden
--- Spacetalk/Step.lean
+++ Spacetalk/Step.lean
@@ -3,56 +3,57 @@ import Spacetalk.Stream
 -- Source Lang
 namespace Step
 
-  ------------------ Syntax ------------------
-
-  inductive Prim
-    | bitVec : Nat → Prim
+------------------ Syntax ------------------
+inductive Prim
+  | bitVec : Nat → Prim
   deriving DecidableEq
 
-  @[reducible] def Prim.denote : Prim → Type
-    | bitVec w => BitVec w
+@[reducible]
+def Prim.denote : Prim → Type
+  | bitVec w => BitVec w
 
-  inductive Ty
-    | stream : Prim → Ty
-    | fn : Ty → Ty → Ty
+inductive Ty
+  | stream : Prim → Ty
   deriving DecidableEq
 
-  infixr:25 " → " => Ty.fn
+infixr:25 " → " => Ty.fn
 
-  @[reducible] def Ty.denote : Ty → Type
-    | stream p => Stream' p.denote
-    | fn α β => α.denote → β.denote
+@[reducible]
+def Ty.denote : Ty → Type
+  | stream p => Stream' p.denote
 
-  inductive BinaryOp : Prim → Prim → Prim → Type
-    | add : {w : Nat} → BinaryOp (.bitVec w) (.bitVec w) (.bitVec w)
-    | mul : {w : Nat} → BinaryOp (.bitVec w) (.bitVec w) (.bitVec w)
+inductive BinaryOp : Prim → Prim → Prim → Type
+  | add : {w : Nat} → BinaryOp (.bitVec w) (.bitVec w) (.bitVec w)
+  | mul : {w : Nat} → BinaryOp (.bitVec w) (.bitVec w) (.bitVec w)
 
-  inductive UnaryOp : Prim → Prim → Type
-    | addConst : {w : Nat} → BitVec w → UnaryOp (.bitVec w) (.bitVec w)
-    | mulConst : {w : Nat} → BitVec w → UnaryOp (.bitVec w) (.bitVec w)
+inductive UnaryOp : Prim → Prim → Type
+  | addConst : {w : Nat} → BitVec w → UnaryOp (.bitVec w) (.bitVec w)
+  | mulConst : {w : Nat} → BitVec w → UnaryOp (.bitVec w) (.bitVec w)
 
-  inductive Prog : Ty → Type
-    | zip : BinaryOp α β γ → Prog (.stream α → .stream β → .stream γ)
-    | map : UnaryOp α β → Prog (.stream α → .stream β)
-    | reduce : BinaryOp α β α → Nat → α.denote → Prog (.stream β → .stream α)
-    | comp : {α β γ : Ty} → Prog (β → γ) → Prog (α → β) → Prog (α → γ)
-    | comp2 : {α β γ δ : Ty} → Prog (γ → δ) → Prog (α → β → γ) → Prog (α → β → δ)
+inductive Prog : Ty → Type
+  | const : (α : Ty) → α.denote → Prog α
+  | zip : BinaryOp α β γ → Prog (.stream α) → Prog (.stream β) → Prog (.stream γ)
+  | map : UnaryOp α β → Prog (.stream α) → Prog (.stream β)
+  | reduce : BinaryOp α β α → Nat → α.denote → Prog (.stream β) → Prog (.stream α)
 
-  ------------------ Semantics ------------------
+------------------ Semantics ------------------
+def BinaryOp.denote : BinaryOp α β γ → α.denote → β.denote → γ.denote
+  | BinaryOp.add => BitVec.add
+  | BinaryOp.mul => BitVec.mul
 
-  def BinaryOp.denote : BinaryOp α β γ → α.denote → β.denote → γ.denote
-    | BinaryOp.add => BitVec.add
-    | BinaryOp.mul => BitVec.mul
+def UnaryOp.denote : UnaryOp α β → α.denote → β.denote
+  | UnaryOp.addConst c => BitVec.add c
+  | UnaryOp.mulConst c => BitVec.mul c
 
-  def UnaryOp.denote : UnaryOp α β → α.denote → β.denote
-    | UnaryOp.addConst c => BitVec.add c
-    | UnaryOp.mulConst c => BitVec.mul c
+def Prog.denote {α : Ty} : Prog α → α.denote
+  | .const _ a => a
+  | .zip op as bs => Stream'.zip op.denote as.denote bs.denote
+  | .map op as => Stream'.map op.denote as.denote
+  | .reduce op n init bs => Stream'.reduce op.denote n init bs.denote
 
-  def Prog.denote {α : Ty} : Prog α → α.denote
-    | Prog.zip op => Stream'.zip op.denote
-    | Prog.map op => Stream'.map op.denote
-    | Prog.reduce op n init => Stream'.reduce op.denote n init
-    | Prog.comp f g => f.denote ∘ g.denote
-    | Prog.comp2 f g => λ a b => f.denote (g.denote a b)
-
+def Prog.p : Prog α → List Nat
+  | .const _ _ => [1]
+  | .zip _ as bs => as.p ++ bs.p
+  | .map _ as => as.p
+  | .reduce _ _ _ bs => bs.p
 end Step
