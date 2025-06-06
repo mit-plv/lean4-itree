a9b55e5771f9f561949a084a8a7b7ceb2ee07cad
Paul Mure <paulmure2000@gmail.com>
2025-06-05 22:47:06 -0700

    fix: par

--- CTree/Par.lean
+++ CTree/Par.lean
@@ -66,9 +66,9 @@ namespace CTree
       | .right t1 t2 =>
         match t2.dest with
         | ⟨.ret _, _⟩ => .inl <| zero
-        | ⟨.tau, c⟩ => .inr <| .inl <| tau' <| rec <| .left t1 (c _fin0)
+        | ⟨.tau, c⟩ => .inr <| .inl <| tau' <| rec <| .right t1 (c _fin0)
         | ⟨.zero, _⟩ => .inl <| zero
-        | ⟨.choice, cts⟩ => .inr <| .inl <| choice' (rec <| .left t1 (cts _fin0)) (rec <| .left t1 (cts _fin1))
+        | ⟨.choice, cts⟩ => .inr <| .inl <| choice' (rec <| .right t1 (cts _fin0)) (rec <| .right t1 (cts _fin1))
         | ⟨.vis _ e, k⟩ =>
           .inr <| .inr <| vis' e λ a =>
             let k := k (.up a)
@@ -76,6 +76,16 @@ namespace CTree
       | .both t1 t2 =>
         match t1.dest, t2.dest with
         | ⟨.ret x, _⟩, ⟨.ret y, _⟩ => .inl <| ret (x, y)
+        /-
+          Note about the choice cases. Our implementation can express infinite non-determinism,
+          which the original Bahr and Hutton formalization cannot. This means that this `both` case
+          is always biased towards the left when destructing choices. However, if the left side contains
+          infinite non-determinism, it does not return. So this should still respect the semantics of the
+          parallel operator. We can maybe get around this by having a case where both sides are choices,
+          but the massive 4-way choice that results from it might not be worth it.
+        -/
+        | ⟨.choice, cts1⟩, _ => .inr <| .inl <| choice' (rec <| .both (cts1 _fin0) t2) (rec <| .both (cts1 _fin1) t2)
+        | _, ⟨.choice, cts2⟩ => .inr <| .inl <| choice' (rec <| .both t1 (cts2 _fin0)) (rec <| .both t1 (cts2 _fin1))
         | ⟨.tau, c1⟩, ⟨.tau, c2⟩ => .inr <| .inl <| tau' <| rec <| .both (c1 _fin0) (c2 _fin0)
         | ⟨.tau, c1⟩, _ => .inr <| .inl <| tau' <| rec <| .both (c1 _fin0) t2
         | _, ⟨.tau, c2⟩ => .inr <| .inl <| tau' <| rec <| .both t1 (c2 _fin0)
