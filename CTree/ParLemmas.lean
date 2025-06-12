3b95dadba6bb6bc839e5ff9438ee31ebd96fc731
Paul Mure <paulmure2000@gmail.com>
2025-06-11 21:48:46 -0700

    par lemmas progress

--- CTree/ParLemmas.lean
+++ CTree/ParLemmas.lean
@@ -1,12 +1,11 @@
 import CTree.Par
 
 namespace CTree
-  theorem par_ret_tau_eq {v : α} {t : CTree ε β}
-    : ret v ‖ t.tau
+  theorem parAux_parS_ret_tau_eq {v : α} {t : CTree ε β}
+    : parAux (.parS (ret v) t.tau)
       = (parAux (ParState.bothS (ret v) t)).tau
         ⊕ zero
         ⊕ (parAux (ParState.rS (ret v) t)).tau := by
-    simp only [par]
     conv =>
       lhs
       rw [parAux_eq_def, parAux_def]
@@ -29,35 +28,64 @@ namespace CTree
       simp only [tau, mk, tau', PFunctor.M.dest_mk]
     simp only [_fin1Const, _fin0, Fin2.ofNat']
 
+  theorem parAux_parS_ret_ret : parAux (.parS (ret (ε := ε) x) (ret y)) = ret (x, y) ⊕ (zero ⊕ zero) := by
+    simp only [parAux]
+    crush_corec_eq
+
+  inductive IsParR (t : CTree ε β) (t1 : CTree ε α) (t2 : CTree ε β) : Prop
+    | lS : t = map Prod.snd (parAux (.lS t1 t2)) → IsParR t t1 t2
+    | rS : t = map Prod.snd (parAux (.rS t1 t2)) → IsParR t t1 t2
+    | lrS : t = map Prod.snd (parAux (.lrS t1 t2)) → IsParR t t1 t2
+    | bothS : t = map Prod.snd (parAux (.bothS t1 t2)) → IsParR t t1 t2
+    | parS : t = map Prod.snd (parAux (.parS t1 t2)) → IsParR t t1 t2
+
   namespace Euttc
     theorem parR_ret : ((ret x) ‖→ t) ≈ t := by
       apply And.intro
-      · apply Refine.coind (λ p1 p2 t1 t2 => ∃ t, t1 = (ret x) ‖→ t ∧ t2 = t) _ ⊤ ⊤
-        -- · repeat apply And.intro rfl
-        · exists t
-        · intro p1 p2 _ t h
-          obtain ⟨_, ht1, ht2⟩ := h
-          -- subst hp1
-          -- subst hp2
-          subst ht1
-          subst ht2
-          apply dMatchOn t
-          · intro v heq
+      · apply Refine.coind (λ p1 p2 t1 t2 => p1 = 0 ∧ p2 = 0 ∧ ∃ t, IsParR t1 (ret x) t ∧ t2 = t) _ 0 0
+        · repeat apply And.intro rfl _
+          exists t
+          apply And.intro _ rfl
+          exact IsParR.parS rfl
+        · intro p1 p2 t1 t2 ⟨hp1, hp2, t, ht1, ht2⟩
+          subst hp1 hp2 ht2
+          match ht1 with
+          | .lS heq => sorry
+          | .rS heq => sorry
+          | .lrS heq => sorry
+          | .bothS heq => sorry
+          | .parS heq =>
             subst heq
-            simp only [parR, par_ret_ret_eq, Functor.map, map_choice, map_ret, map_zero]
-            apply RefineF.choice_idemp
-            · exact RefineF.ret rfl
-            · apply RefineF.choice_idemp
-              <;> exact RefineF.zero
-          · intro c heq
-            subst heq
-            simp only [parR]
-            rw [par_ret_tau_eq]
-            
-            sorry
-          · sorry
-          · sorry
-          · sorry
+            apply dMatchOn t2
+            · intro v heq
+              subst heq
+              simp only [parAux_parS_ret_ret, map_choice, map_ret, map_zero]
+              apply RefineF.choice_idemp
+              · exact RefineF.ret rfl
+              · apply RefineF.choice_idemp
+                <;> exact RefineF.zero
+            · intro c heq
+              subst heq
+              simp only [parAux_parS_ret_tau_eq, map_choice, map_tau, map_zero]
+              apply RefineF.tau_right
+              apply RefineF.choice_idemp
+              · apply RefineF.tau_left
+                apply RefineF.coind 0 0 ENat.top_pos ENat.top_pos
+                repeat apply And.intro rfl _
+                exists c
+                apply And.intro _ rfl
+                exact IsParR.bothS rfl
+              · apply RefineF.choice_idemp
+                · exact RefineF.zero
+                · apply RefineF.tau_left
+                  apply RefineF.coind 0 0 ENat.top_pos ENat.top_pos
+                  repeat apply And.intro rfl _
+                  exists c
+                  apply And.intro _ rfl
+                  exact IsParR.rS rfl
+            · sorry
+            · sorry
+            · sorry
       · sorry
 
     theorem parR_map : ((map f t1) ‖→ t2) ≈ (t1 ‖→ t2) := by
