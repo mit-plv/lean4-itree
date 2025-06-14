e737623d3467778d6407922debc65473b1e06061
Paul Mure <paulmure2000@gmail.com>
2025-06-13 22:31:35 -0700

    fix: weak bisim

--- CTree/SemanticEquiv.lean
+++ CTree/SemanticEquiv.lean
@@ -3,7 +3,14 @@ import CTree.LTS
 
 namespace CTree
 
+  theorem refine_of_weak_sim {sim : Rel (CTree ε ρ) (CTree ε ρ)} {t1 t2 : CTree ε ρ}
+    (hsim : IsWeakBisimulation sim) (h : sim t1 t2)
+    : t1 ≤Eq≤ t2 := by
+    sorry
+
   theorem euttc_of_lts {t1 t2 : CTree ε ρ} (h : WeakBisim t1 t2) : t1 ≈ t2 := by
+    obtain ⟨sim, ⟨⟨hsim1, hsim2⟩, h⟩⟩ := h
+
     sorry
 
   theorem lts_of_euttc {t1 t2 : CTree ε ρ} (h : t1 ≈ t2) : WeakBisim t1 t2 := by
