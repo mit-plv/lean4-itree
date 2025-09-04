88cc2122e198967a36f72c1d265b7d5c8018da1e
Paul Mure <paulmure2000@gmail.com>
2025-09-03 23:56:56 -0400

    refactor: update effect algebra

--- ITree/EffectAlgebra.lean
+++ ITree/EffectAlgebra.lean
@@ -1,4 +1,4 @@
-import CTree.Monad
+import ITree.Monad
 
 abbrev parametricFun (E : Type u → Type v1) (F : Type u → Type v2) :=
   ∀ {α : Type u}, E α → F α
@@ -11,13 +11,13 @@ inductive SumE (ε1 ε2 : Type u → Type v) : Type u → Type v
 instance : Add (Type u → Type v) where
   add := SumE
 
-namespace CTree
+namespace ITree
 
   inductive iterState {ε ρ ι}
   | iterS (i : ι)
-  | bindS (t : CTree ε (ι ⊕ ρ))
+  | bindS (t : ITree ε (ι ⊕ ρ))
 
-  def iter {ε ρ ι} (step : ι → CTree ε (ι ⊕ ρ)) (i : ι) : CTree ε ρ :=
+  def iter {ε ρ ι} (step : ι → ITree ε (ι ⊕ ρ)) (i : ι) : ITree ε ρ :=
     .corec' (fun rec (s : iterState) =>
       match s with
       | .bindS t =>
@@ -26,21 +26,17 @@ namespace CTree
           match v with
           | .inl l => .inr <| tau' <| rec <| .bindS (step l)
           | .inr r => .inl <| ret r
-        | ⟨.tau, c⟩ => .inr <| tau' <| rec <| .bindS <| c _fin0
+        | ⟨.tau, c⟩ => .inr <| tau' <| rec <| .bindS <| c 0
         | ⟨.vis _ e, k⟩ => .inr <| vis' e <| fun a => rec <| .bindS <| k a
-        | ⟨.zero, _⟩ => .inr <| zero'
-        | ⟨.choice, cts⟩ => .inr <| choice' (rec <| .bindS <| cts _fin0) (rec <| .bindS <| cts _fin1)
       | .iterS i => .inr <| tau' <| rec <| .bindS (step i)
     ) (.iterS i)
 
   def interp {ε1 : Type u1 → Type v1} {ε2 : Type u2 → Type v2}
-    (handler : ε1 ⟹ CTree ε2) {ρ : Type v3} (t : CTree ε1 ρ) : CTree ε2 ρ :=
+    (handler : ε1 ⟹ ITree ε2) {ρ : Type v3} (t : ITree ε1 ρ) : ITree ε2 ρ :=
     iter (fun t =>
       match t.dest with
       | ⟨.ret v, _⟩ => ret <| .inr v
-      | ⟨.tau, c⟩ => ret <| .inl (c _fin0)
+      | ⟨.tau, c⟩ => ret <| .inl (c 0)
       | ⟨.vis _ e, k⟩ => (handler e).bind <| fun a => ret <| .inl <| k a -- using >>= messes up universes
-      | ⟨.zero, _⟩ => zero
-      | ⟨.choice, cts⟩ => ret <| .inl <| (cts _fin0) ⊕ (cts _fin1)
     ) t
-end CTree
+end ITree
