64426260ffbb1eb6b3f56784d7bd611b78a022b7
Joonhyup Lee <joonhyup@mit.edu>
2025-09-11 06:50:05 -0400

    feat: interp with explicit universes only at the return type

--- ITree/EffectAlgebra.lean
+++ ITree/EffectAlgebra.lean
@@ -1,9 +1,7 @@
 import ITree.Monad
 
--- ε1 usually takes the answer type or the return type
--- ε2 is usually some iterative monad m ≥ max (a + 1) e r
-abbrev naturalTransformation (ε1 : Type u → Type v) (ε2 : Type max _ u → Type w) :=
-  ∀ {α}, ε1 α → ε2 (ULift.{max _ u, _} α)
+abbrev naturalTransformation (ε1 : Type r → Type u) (ε2 : Type max _ r → Type v) :=
+  ∀ {ρ}, ε1 ρ → ε2 (ULift.{max _ r, _} ρ)
 infixr:50 " ⟶ "=> naturalTransformation
 
 inductive SumE (ε1 ε2 : Type u → Type v) : Type u → Type v
@@ -32,13 +30,16 @@ inductive IterState (ι : Type u) (ρ : Type v)
 class MonadIter (m : Type max r i → Type v) where
   iter {ρ : Type r} {ι : Type i} : (ι → m (IterState ι ρ)) → ι → m (ULift ρ)
 
+abbrev handler (ε : Type a → Type e) m [MonadIter.{max (a + 1) e r, r, _} m] :=
+  ∀ {α}, ε α → m (ULift.{max (a + 1) e r, _} α)
+
 namespace ITree
 
 inductive IterMode {ε ρ ι}
   | iterS (i : ι)
   | bindS (t : ITree ε (IterState ι ρ))
 
-def iter {ε : Type u → Type v} {ρ : Type r} {ι : Type i}
+def iter {ε : Type a → Type e} {ρ : Type r} {ι : Type i}
   (step : ι → ITree ε (IterState ι ρ)) (i : ι) : ITree ε (ULift ρ) :=
   .corec' (fun rec (s : IterMode) =>
     match s with
@@ -56,15 +57,14 @@ def iter {ε : Type u → Type v} {ρ : Type r} {ι : Type i}
 instance {ε : Type a → Type e} : MonadIter (ITree ε) where
   iter := ITree.iter
 
-def interp {ε : Type a → Type e} [Monad m] [MonadIter.{max (a + 1) e r, r, _} m]
-  (handler : ∀ {α}, ε α → m (ULift.{max (a + 1) e r, _} α))
-  : ITree.{a, e, r} ε ⟶ m :=
+def interp {ε : Type a → Type e} [Monad m] [MonadIter.{_, r, _} m] (h : handler ε m)
+  : ITree.{_, _, r} ε ⟶ m :=
   MonadIter.iter (fun t =>
     match t.dest with
     | ⟨.ret v, _⟩ => return .done v
     | ⟨.tau, c⟩ => return .recur <| c 0
     | ⟨.vis _ e, k⟩ => do
-      let a ← handler e
+      let a ← h e
       return .recur <| k a.down
   )
 
